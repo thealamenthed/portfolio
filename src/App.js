986c7f0256b584f61ee76f5c88f7b050e2da5da9
thealamenthed <dalila.boualemfr@gmail.com>
2022-10-10 15:21:57 +0200

    add component testimonials

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
+import Testimonials from "./components/testimonials/Testimonials";
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
       <Skills />
       <Services />
       <Qualification />
+      <Testimonials />
     </main>
     </>
   )
--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import "./qualification.css";
 
 const Qualification = () => {
-    const [toggleState, setToggleState] = useState(1);
+  const [toggleState, setToggleState] = useState(0);
 
-    const toggleTab = (index) => {
-        setToggleState(index);
-    }
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <section className="qualification section">
       <h2 className="section__title ">Qualification</h2>
@@ -14,22 +14,39 @@ const Qualification = () => {
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
-          onClick={() => toggleTab(1)}
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
           >
             <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
 
-          <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
-          onClick={() => toggleTab(2)}>
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
             <i className="uil uil-briefcase-alt qualification__icon"></i>
             Experience
           </div>
         </div>
 
         <div className="qualification__sections">
-          <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">Web Design</h3>
@@ -66,18 +83,20 @@ const Qualification = () => {
             </div>
 
             <div className="qualification__data">
-                <div>
-                    <h3 className="qualification__title">Web Development</h3>
-                    <span className="qualification__subtitle">Spain - Institute</span>
-                    <div className="qualification__calender">
-                        <i className="uil uil-calendar-alt"></i> 2021 - Present
-                    </div>
+              <div>
+                <h3 className="qualification__title">Web Development</h3>
+                <span className="qualification__subtitle">
+                  Spain - Institute
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
                 </div>
+              </div>
 
-                <div>
-                    <span className="qualification__rounder"></span>
-                    <span className="qualification__line"></span>
-                </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
             </div>
 
             <div className="qualification__data">
@@ -90,16 +109,23 @@ const Qualification = () => {
 
               <div>
                 <h3 className="qualification__title">UX Expert</h3>
-                <span className="qualification__subtitle">Spain - Institute</span>
+                <span className="qualification__subtitle">
+                  Spain - Institute
+                </span>
                 <div className="qualification__calender">
-                    <i className="uil uil-calendar-alt"></i> 2017 - 2018
+                  <i className="uil uil-calendar-alt"></i> 2017 - 2018
                 </div>
+              </div>
             </div>
-            
-            </div>
-        </div>
+          </div>
 
-        <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">Product Designer</h3>
@@ -136,24 +162,22 @@ const Qualification = () => {
             </div>
 
             <div className="qualification__data">
-                <div>
-                    <h3 className="qualification__title">Web Designer</h3>
-                    <span className="qualification__subtitle">Figma - Spain</span>
-                    <div className="qualification__calender">
-                        <i className="uil uil-calendar-alt"></i> 2021 - Present
-                    </div>
+              <div>
+                <h3 className="qualification__title">Web Designer</h3>
+                <span className="qualification__subtitle">Figma - Spain</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
                 </div>
+              </div>
 
-                <div>
-                    <span className="qualification__rounder"></span>
-                    <span className="qualification__line"></span>
-                </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
             </div>
+          </div>
         </div>
-
-
       </div>
-    </div>
     </section>
   );
 };
